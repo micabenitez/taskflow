1ba3c5e586ab55e760583cccc98de850a6cd1067
micaela benitez <mabenittez@gmail.com>
2026-03-04 21:47:23 -0300

    feat: create serializers with nested relationships

--- core/serializers.py
+++ core/serializers.py
@@ -1,1 +1,22 @@
 from rest_framework import serializers
+from .models import Board, Column, Card
+
+class CardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Card
+        fields = ['id', 'title', 'description', 'created_at', 'due_date', 'column']
+
+class ColumnSerializer(serializers.ModelSerializer):
+    cards = CardSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Column
+        fields = ['id', 'name', 'position', 'board', 'cards']
+
+class BoardSerializer(serializers.ModelSerializer):
+    columns = ColumnSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Board
+        fields = ['id', 'name', 'description', 'created_at', 'owner', 'columns']
+        read_only_fields = ['owner']
